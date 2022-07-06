809ac3e7686305530011fe2ac468b051704c38fe
Steven Garcia <Garciaintech@gmail.com>
2022-07-06 15:09:06 -0400

    Add get by id method

--- src/backend/controller/plantController.js
+++ src/backend/controller/plantController.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
 
 var { Plant } = require('../models/plant');
 
 
 // => localhost:8000/plants
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     Plant.find((err, docs) => {
         if(!err) {
             res.send(docs);
         } else {
-            console.log('Error in Retrieving Plant Data :' + JSON.stringify(err, undefined, 2));
+            console.log('Error in Retrieving Plants :' + JSON.stringify(err, undefined, 2));
+        }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    if(!ObjectId.isValid(req.params.id)){
+        return res.status(400).send(`No plantwith given id : ${req.params.id}`);
+    }
+
+    Plant.findById(req.params.id, (err, doc) => {
+        if(!err) {
+            res.send(docs);
+        } else {
+            console.log('Error in Retrieving Plant :' + JSON.stringify(err, undefined, 2));
         }
     });
 });
