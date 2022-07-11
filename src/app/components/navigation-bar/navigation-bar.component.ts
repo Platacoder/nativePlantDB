5e6d86f635a67869f10b471eeceb4b7fbd217a1f
Steven Garcia <Garciaintech@gmail.com>
2022-07-11 09:47:12 -0400

    add color palette doc

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PlantService } from 'src/app/services/plant.service';
 
 @Component({
