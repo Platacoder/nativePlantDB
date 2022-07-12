6b6b60c13bc6b77e6c80ba825d20fd9865ab632f
Steven Garcia <Garciaintech@gmail.com>
2022-07-12 09:51:45 -0400

    build welcome-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { WelcomeCardComponent } from './components/welcome-card/welcome-card.component';
 import { HomeComponent } from './pages/home/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    HomeComponent
+    HomeComponent,
+    WelcomeCardComponent
   ],
   imports: [
     BrowserModule,
